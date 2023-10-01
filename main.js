c55d9c0ed4a4d3d814bed1a1f52493f18c372c16
sahal-shalu <sahalshalu5426@gmail.com>
2023-10-02 04:57:32 +0530

    image and forms uploaded completed

--- main.js
+++ main.js
@@ -30,6 +30,7 @@ app.use((req,res,next)=>{
     next()
 })
 
+app.use(express.static('uploads'))
 
 app.set('view engine','ejs');
 //rout prefix;
--- routes/routes.js
+++ routes/routes.js
@@ -18,34 +18,70 @@ var storage=multer.diskStorage({
 
  //insert an user into database route
 
- router.post('/add',upload,(req,res)=>{
-    const user=new User({
-        name:req.body.name,
-        email:req.body.email,
-        phone:req.body.phone,
-        image:req.file.filename,
-    })
-    user.save((err)=>{
-        if(err){
-            res.json({message:err.message,type:'danger'})
-        }else{
-            req.session.message={
-                type:'success',
-                message:'user added successully '
-            };
-            res.redirect("/")
-        }
-    })
- })
+ router.post('/add', upload, (req, res) => {
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        phone: req.body.phone,
+        image: req.file.filename,
+    });
 
-router.get('/',(req,res)=>{
-    res.render('index',{title:"Home page"})
-})
+    user.save()
+        .then(() => {
+            req.session.message = {
+                type: 'success',
+                message: 'User added successfully',
+            };
+            res.redirect('/');
+        })
+        .catch((err) => {
+            res.json({ message: err.message, type: 'danger' });
+        });
+});
+
+
+//get all users
+
+router.get('/', (req, res) => {
+    User.find()
+        .exec()  // Remove the callback function from here
+        .then((users) => {
+            res.render("index", {
+                title: 'Home page',
+                users: users,
+            });
+        })
+        .catch((err) => {
+            res.json({ message: err.message });
+        });
+});
+
 
 router.get('/add',(req,res)=>{
     res.render('add_users',{title:"Add Users"})
 })
 
+
+//Edit an user
+
+router.get('/edit/:id',(req,res)=>{
+    let id=req.params.id;
+    user.findById(id,(err,user)=>{
+        if(err){
+            res.redirect('/')
+
+        }else{
+            if(user==null){
+                res.redirect('/')
+            }else{
+                res.render('edit_users',{
+                    title:'Edit user',
+                     user:user,
+                })
+            }
+        }
+    })
+})
 
 
 
